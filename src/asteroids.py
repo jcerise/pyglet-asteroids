29300b5eea4fd17db4b8eb27748cb60f945875c2
Jeremy Cerise <jcerise06@gmail.com>
2015-02-24 21:32:24 -0700

    Added PhysicalObject class, which is a subclass of Sprite. This allows for all sprites to share movement code. Made the meteors move around the screen

--- src/asteroids.py
+++ src/asteroids.py
@@ -1,29 +1,38 @@
 import pyglet
-from game import resources, load
+from game import resources, load, physicalobject
 
 # Set the main game window
 game_window = pyglet.window.Window(800, 600)
 
+# Define the main graphics batch
+main_batch = pyglet.graphics.Batch()
+
 # Set the main labels that will display at the top of the screen
-score_label = pyglet.text.Label(text="Score: 0", x=10, y=575)
-level_label = pyglet.text.Label(text="Pyglet Asteroids", x=400, y=575, anchor_x='center')
+score_label = pyglet.text.Label(text="Score: 0", x=10, y=575, batch=main_batch)
+level_label = pyglet.text.Label(text="Pyglet Asteroids", x=400, y=575, anchor_x='center', batch=main_batch)
+player_lives = load.player_lives(3, main_batch)
 
 # Set up the player
-player_ship = pyglet.sprite.Sprite(img=resources.player_image, x=400, y=300)
+player_ship = physicalobject.PhysicalObject(img=resources.player_image, x=400, y=300, batch=main_batch)
 
 # Set up the asteroids
-asteroids = load.asteroids(3, player_ship.position)
+asteroids = load.asteroids(3, player_ship.position, main_batch)
+
+# Create a list of all game objects
+game_objects = [player_ship] + asteroids
+
+
+def update(dt):
+    for obj in game_objects:
+        obj.update(dt)
+
 
 @game_window.event
 def on_draw():
-    # Draw
+    # Draw all of our batched sprites to the screen
     game_window.clear()
-    level_label.draw()
-    score_label.draw()
-    player_ship.draw()
-
-    for asteroid in asteroids:
-        asteroid.draw()
+    main_batch.draw()
 
 if __name__ == '__main__':
+    pyglet.clock.schedule_interval(update, 1/120.0)
     pyglet.app.run()
--- src/game/load.py
+++ src/game/load.py
@@ -2,6 +2,7 @@ import pyglet
 import random
 import math
 import resources
+import physicalobject
 
 
 def distance(point_1=(0, 0), point_2=(0, 0)):
@@ -16,12 +17,31 @@ def distance(point_1=(0, 0), point_2=(0, 0)):
     )
 
 
-def asteroids(num_asteroids, player_position):
+def player_lives(num_icons, batch=None):
     """
-    Creates num_asteroids asteroid sprites at random positions and rotations, checking to ensure they are far enough
+    Create num_icons player lives sprites. These are displayed in the top right of the screen, and indicate how many
+    lives the player has.
+    :param num_icons: The number of player_lives icons to display
+    :param batch: The drawing batch to include the player_lives in
+    :return: A list of player_lives sprites
+    """
+    player_lives = []
+    for i in range(num_icons):
+        new_sprite = pyglet.sprite.Sprite(img=resources.player_image, x=785-i*30, y=585, batch=batch)
+        new_sprite.scale = 0.5
+        new_sprite.rotation = 270
+        player_lives.append(new_sprite)
+
+    return player_lives
+
+
+def asteroids(num_asteroids, player_position, batch=None):
+    """
+    Creates num_asteroids asteroid PhysicalObjects at random positions and rotations, checking to ensure they are far enough
     away from the players location.
     :param num_asteroids: The number of asteroids to draw to the screen
     :param player_position: The players current position
+    :param batch: The batch to include the asteroids in
     :return: A list of asteroids sprites
     """
     asteroids = []
@@ -32,7 +52,10 @@ def asteroids(num_asteroids, player_position):
             asteroid_x = random.randint(0, 800)
             asteroid_y = random.randint(0, 600)
 
-        new_asteroid = pyglet.sprite.Sprite(img=resources.asteroid_image, x=asteroid_x, y=asteroid_y)
+        new_asteroid = physicalobject.PhysicalObject(img=resources.asteroid_image, x=asteroid_x, y=asteroid_y,
+                                                     batch=batch)
         new_asteroid.rotation = random.randint(0, 360)
+        new_asteroid.velocity_x = random.random() * 40
+        new_asteroid.velocity_y = random.random() * 40
         asteroids.append(new_asteroid)
     return asteroids
